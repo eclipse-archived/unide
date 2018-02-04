c58a14a6dc4c85870cae1c8bfd711cf8c5d0c96f
Axel Meinhardt <axel.meinhardt@bosch-si.com>
2018-02-04 21:43:14 +0100

    feat(flyway): replaced liquibase with flyway and added h2 support
    Signed-off-by: Axel Meinhardt <axel.meinhardt@bosch-si.com>

---
+++ servers/rest/src/main/java/org/eclipse/iot/unide/server/receiver/sql/JdbcExecutorService.java
@@ -0,0 +1,112 @@
+/*
+ * Copyright (c) 2018 Bosch Software Innovations GmbH. All rights reserved.
+ */
+
+package org.eclipse.iot.unide.server.receiver.sql;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.flywaydb.core.Flyway;
+
+/**
+ * Service to execute JDBC statements. Exception handling and commits are done
+ * by this service.
+ *
+ * Provides method for execution of flyway changelog.
+ */
+public class JdbcExecutorService {
+
+	private static final String FLYWAY_BASE = "org/eclipse/iot/unide/server/receiver/sql/migrations";
+	private static final String FLYWAY_TABLE = "SCHEMA_VERSION";
+
+	private DataSource dataSource;
+
+	public JdbcExecutorService(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
+
+	/**
+	 * The given runner will be executed by this method by passing a valid jdbc
+	 * connection. The caller does not have handle connection related actions.
+	 * Commit, close and exception handling is done by this method.
+	 *
+	 * @param runner
+	 *            executes the given runner
+	 */
+	public void execute(ExecutorCommand<Connection> runner) {
+		Connection connection = null;
+		try {
+			connection = getConnection();
+			connection.setAutoCommit(false);
+			runner.accept(connection);
+			connection.commit();
+		} catch (Exception ex) {
+			throw new RuntimeException("Data access failed.", ex);
+		} finally {
+			try {
+				if (connection != null) {
+					connection.close();
+				}
+			} catch (SQLException e) {
+				// ignore
+			}
+		}
+	}
+
+	private Connection getConnection() throws SQLException {
+		return dataSource.getConnection();
+	}
+
+	@FunctionalInterface
+	public interface ExecutorCommand<T> {
+		void accept(T t) throws Exception;
+	}
+
+	/**
+	 * Executes the database changelog using flyway.
+	 */
+	void executeChangeLog() {
+		Flyway flyway = new Flyway();
+		flyway.setTable(FLYWAY_TABLE);
+		flyway.setDataSource(dataSource);
+		flyway.setLocations(FLYWAY_BASE + "/" + getDbType());
+		flyway.setBaselineOnMigrate(true);
+		// flyway.setBaselineVersionAsString( "0" );
+		flyway.migrate();
+	}
+
+	/**
+	 * Returns the database type, if known.
+	 *
+	 * @return String which is database type
+	 */
+	public String getDbType() {
+		String dbName = "unknown";
+		String dbType;
+		try {
+			dbName = dataSource.getConnection().getMetaData().getDatabaseProductName();
+		} catch (SQLException e) {
+            throw new RuntimeException( "Failed to get connection", e );
+		}
+		switch (dbName) {
+		case "PostgreSQL":
+		case "MockDatabase":
+			dbType = "postgres";
+			break;
+		case "H2":
+			dbType = "h2";
+			break;
+		case "Microsoft SQL Server":
+		case "Oracle":
+		case "MySQL":
+		case "HSQL Database Engine":
+		default:
+			throw new RuntimeException("Unknown database '" + dbName + "'");
+		}
+		return dbType;
+	}
+}
---
+++ servers/rest/src/main/java/org/eclipse/iot/unide/server/receiver/sql/MeasurementConsumer.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2018 Bosch Software Innovations GmbH. All rights reserved.
+ */
+
+package org.eclipse.iot.unide.server.receiver.sql;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.List;
+
+import org.eclipse.iot.unide.ppmp.commons.Device;
+import org.eclipse.iot.unide.ppmp.measurements.Measurements;
+import org.eclipse.iot.unide.ppmp.measurements.MeasurementsWrapper;
+import org.eclipse.iot.unide.server.receiver.util.PpmpHelper;
+
+/**
+ * Consumer class for Measurement objects
+ */
+class MeasurementConsumer extends AbstractSqlConsumer<MeasurementsWrapper> {
+
+   private static final String INSERT_MEASUREMENT =
+         "INSERT INTO ppmp_measurements(time, deviceid, measurementpoint, value) VALUES(?,?,?,?)";
+
+   MeasurementConsumer( JdbcExecutorService executorService ) {
+      super( executorService );
+   }
+
+   @Override
+   public void accept( MeasurementsWrapper data ) {
+      Device device = data.getDevice();
+      data.getMeasurements().forEach( measurement -> insertSingleMeasurement( measurement, device ) );
+   }
+
+   private void insertSingleMeasurement( Measurements measurements, Device device ) {
+      List<PpmpHelper.MeasurementValue> measurementValues = PpmpHelper.getMeasurementValues( measurements );
+      final String deviceID = device.getDeviceID();
+      getExecutorService().execute( connection ->
+            measurementValues.forEach( measurement -> {
+                     try {
+                        PreparedStatement statement = connection.prepareStatement( INSERT_MEASUREMENT );
+                        statement.setTimestamp( 1, toTimeStamp( measurement.getTime() ) );
+                        statement.setString( 2, deviceID );
+                        statement.setString( 3, measurement.getName() );
+                        statement.setDouble( 4, measurement.getValue() );
+                        statement.executeUpdate();
+                     } catch ( SQLException e ) {
+                        throw new RuntimeException( e );
+                     }
+                  }
+            )
+      );
+   }
+
+   private static Timestamp toTimeStamp( long timestamp ) {
+      return new Timestamp( timestamp );
+   }
+
+}
