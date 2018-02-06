6fab98f8ae86a305cc40bb150be0a5755288fc5f
Axel Meinhardt <axel.meinhardt@bosch-si.com>
2018-02-06 14:22:45 +0100

    fix(tests): replacing okhttp with apache client
    Signed-off-by: Axel Meinhardt <axel.meinhardt@bosch-si.com>

--- servers/rest/src/test/java/org/eclipse/iot/unide/server/jdbc/JdbcMockFactory.java
+++ servers/rest/src/test/java/org/eclipse/iot/unide/server/jdbc/JdbcMockFactory.java
@@ -8,7 +8,6 @@ import java.io.IOException;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
-import java.nio.charset.StandardCharsets;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.util.Arrays;
@@ -21,192 +20,189 @@ import com.mockrunner.mock.jdbc.MockConnection;
 import com.mockrunner.mock.jdbc.MockPreparedStatement;
 import com.mockrunner.mock.jdbc.ParameterReference;
 
-import okhttp3.Headers;
-import okhttp3.MediaType;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
-import okio.Buffer;
+import org.apache.http.HttpEntity;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.NoConnectionReuseStrategy;
+import org.apache.http.util.EntityUtils;
+
 
 /**
- * Provides the possibility to mock and stub JDBC statements.
- * Currently only INSERT statements are supported.
+ * Provides the possibility to mock and stub JDBC statements. Currently only
+ * INSERT statements are supported.
  *
- * Statements are delegated to a http server. The http server can provide stub data based on the received statement.
- * This can be archived for instance using WireMock.
+ * Statements are delegated to a http server. The http server can provide stub
+ * data based on the received statement. This can be archived for instance using
+ * WireMock.
  */
 public class JdbcMockFactory {
 
-   public static Connection getMockConnection( String stubUrl ) {
-      return (Connection) Proxy.newProxyInstance( Connection.class.getClassLoader(), new Class[] { Connection.class },
-            new ConnectionInvocationHandler( stubUrl, new MockConnection() ) );
-   }
+	public static Connection getMockConnection(String stubUrl) {
+		return (Connection) Proxy.newProxyInstance(Connection.class.getClassLoader(), new Class[] { Connection.class },
+				new ConnectionInvocationHandler(stubUrl, new MockConnection()));
+	}
 
-   private static class BaseMethodInvocationHandler implements InvocationHandler {
-      private final Object target;
-      private final Map<MethodKey, Method> methods;
+	private static class BaseMethodInvocationHandler implements InvocationHandler {
+		private final Object target;
+		private final Map<MethodKey, Method> methods;
 
-      BaseMethodInvocationHandler( Object target ) {
-         this.target = target;
-         this.methods = getMethods( target.getClass() );
-      }
+		BaseMethodInvocationHandler(Object target) {
+			this.target = target;
+			this.methods = getMethods(target.getClass());
+		}
 
-      public Object invoke( Object proxy, Method method, Object[] args ) throws Throwable {
-         if ( args == null || args.length == 0 ) {
-            return methods.get( new MethodKey( method.getName(), method.getParameterTypes() ) ).invoke( target );
-         }
-         return methods.get( new MethodKey( method.getName(), method.getParameterTypes() ) )
-                       .invoke( target, args );
-      }
+		public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+			if (args == null || args.length == 0) {
+				return methods.get(new MethodKey(method.getName(), method.getParameterTypes())).invoke(target);
+			}
+			return methods.get(new MethodKey(method.getName(), method.getParameterTypes())).invoke(target, args);
+		}
 
-      private static Map<MethodKey, Method> getMethods( Class<?> clazz ) {
-         Method[] methods = clazz.getMethods();
-         return Arrays.stream( methods ).collect(
-               Collectors
-                     .toMap( entry -> new MethodKey( entry.getName(), entry.getParameterTypes() ), entry -> entry ) );
-      }
+		private static Map<MethodKey, Method> getMethods(Class<?> clazz) {
+			Method[] methods = clazz.getMethods();
+			return Arrays.stream(methods).collect(Collectors
+					.toMap(entry -> new MethodKey(entry.getName(), entry.getParameterTypes()), entry -> entry));
+		}
 
-      private static class MethodKey {
-         final String methodName;
-         final Class<?>[] parameterTypes;
+		private static class MethodKey {
+			final String methodName;
+			final Class<?>[] parameterTypes;
 
-         MethodKey( String methodName, Class[] parameterTypes ) {
-            this.methodName = methodName;
-            this.parameterTypes = parameterTypes;
-         }
+			MethodKey(String methodName, Class[] parameterTypes) {
+				this.methodName = methodName;
+				this.parameterTypes = parameterTypes;
+			}
 
-         @Override
-         public boolean equals( Object o ) {
-            if ( this == o ) {
-               return true;
-            }
-            if ( !(o instanceof MethodKey) ) {
-               return false;
-            }
-            MethodKey methodKey = (MethodKey) o;
-            if ( !methodName.equals( methodKey.methodName ) ) {
-               return false;
-            }
-            return Arrays.equals( parameterTypes, methodKey.parameterTypes );
-         }
+			@Override
+			public boolean equals(Object o) {
+				if (this == o) {
+					return true;
+				}
+				if (!(o instanceof MethodKey)) {
+					return false;
+				}
+				MethodKey methodKey = (MethodKey) o;
+				if (!methodName.equals(methodKey.methodName)) {
+					return false;
+				}
+				return Arrays.equals(parameterTypes, methodKey.parameterTypes);
+			}
 
-         @Override
-         public int hashCode() {
-            int result = methodName.hashCode();
-            result = 31 * result + Arrays.hashCode( parameterTypes );
-            return result;
-         }
-      }
-   }
+			@Override
+			public int hashCode() {
+				int result = methodName.hashCode();
+				result = 31 * result + Arrays.hashCode(parameterTypes);
+				return result;
+			}
+		}
+	}
 
-   private static class ConnectionInvocationHandler extends BaseMethodInvocationHandler {
+	private static class ConnectionInvocationHandler extends BaseMethodInvocationHandler {
 
-      private final Connection connection;
-      private final Map<String, Method> methods;
-      private final String stubUrl;
+		private final Connection connection;
+		private final Map<String, Method> methods;
+		private final String stubUrl;
 
-      ConnectionInvocationHandler( String stubUrl, Connection connection ) {
-         super( connection );
-         this.stubUrl = stubUrl;
-         this.connection = connection;
-         this.methods = new HashMap<>();
-         Arrays.stream( connection.getClass().getMethods() )
-               .forEach( method -> this.methods.put( method.getName(), method ) );
-      }
+		ConnectionInvocationHandler(String stubUrl, Connection connection) {
+			super(connection);
+			this.stubUrl = stubUrl;
+			this.connection = connection;
+			this.methods = new HashMap<>();
+			Arrays.stream(connection.getClass().getMethods())
+					.forEach(method -> this.methods.put(method.getName(), method));
+		}
 
-      @Override
-      public Object invoke( Object proxy, Method method, Object[] args ) throws Throwable {
-         if ( "prepareStatement".equals( method.getName() ) ) {
-            String sqlStatement = (String) args[0];
-            return Proxy.newProxyInstance( PreparedStatement.class.getClassLoader(),
-                  new Class[] { PreparedStatement.class },
-                  new MockPreparedStatementHandler( stubUrl, new MockPreparedStatement( connection, sqlStatement ) ) );
-         }
-         return super.invoke( proxy, method, args );
-      }
+		@Override
+		public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+			if ("prepareStatement".equals(method.getName())) {
+				String sqlStatement = (String) args[0];
+				return Proxy.newProxyInstance(PreparedStatement.class.getClassLoader(),
+						new Class[] { PreparedStatement.class },
+						new MockPreparedStatementHandler(stubUrl, new MockPreparedStatement(connection, sqlStatement)));
+			}
+			return super.invoke(proxy, method, args);
+		}
 
-   }
+	}
 
-   private static class MockPreparedStatementHandler extends BaseMethodInvocationHandler {
+	private static class MockPreparedStatementHandler extends BaseMethodInvocationHandler {
 
-      private final MockPreparedStatement mockPreparedStatement;
-      private final String stubUrl;
-      private final MediaType RESPONSE_MEDIA_TYPE = MediaType.parse( "application/text" );
+		private final MockPreparedStatement mockPreparedStatement;
+		private final String stubUrl;
 
-      MockPreparedStatementHandler( String stubUrl, MockPreparedStatement mockPreparedStatement ) {
-         super( mockPreparedStatement );
-         this.stubUrl = stubUrl;
-         this.mockPreparedStatement = mockPreparedStatement;
-      }
+		MockPreparedStatementHandler(String stubUrl, MockPreparedStatement mockPreparedStatement) {
+			super(mockPreparedStatement);
+			this.stubUrl = stubUrl;
+			this.mockPreparedStatement = mockPreparedStatement;
+		}
 
-      @Override
-      public Object invoke( Object proxy, Method method, Object[] args ) throws Throwable {
-         if ( "executeUpdate".equals( method.getName() ) ) {
-            String responseBody = sendRequest();
-            return parseToUpdateResult( responseBody );
-         }
-         return super.invoke( proxy, method, args );
-      }
+		@Override
+		public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+			if ("executeUpdate".equals(method.getName())) {
+				String responseBody = sendRequest();
+				return parseToUpdateResult(responseBody);
+			}
+			return super.invoke(proxy, method, args);
+		}
 
-      private static int parseToUpdateResult( String responseBody ) {
-         try {
-            return Integer.parseInt( responseBody );
-         } catch ( NumberFormatException e ) {
-            throw new RuntimeException( "For executeUpdate only Number are possible. Value was: '" + responseBody + "'",
-                  e );
-         }
-      }
+		private static int parseToUpdateResult(String responseBody) {
+			try {
+				return Integer.parseInt(responseBody);
+			} catch (NumberFormatException e) {
+				throw new RuntimeException(
+						"For executeUpdate only Number are possible. Value was: '" + responseBody + "'", e);
+			}
+		}
 
-      private String sendRequest() throws IOException {
-         Request.Builder post = new Request.Builder()
-               .url( stubUrl )
-               .headers( Headers.of( getIndexedParameters() ) )
-               .post( RequestBody.create( RESPONSE_MEDIA_TYPE, mockPreparedStatement.getSQL() ) );
-         OkHttpClient okHttpClient = new OkHttpClient();
-         Request request = post.build();
-         Response response = okHttpClient.newCall( request ).execute();
-         if ( response.isSuccessful() ) {
-            return new String( response.body().bytes(), StandardCharsets.UTF_8 );
-         } else {
-            throw new RuntimeException(
-                  "No stub for url: '" + request.url() + "' body: '" + bodyToString( request ) + "' headers: '"
-                        + request.headers() + "' found." );
-         }
-      }
+		private String sendRequest() throws IOException {
+			HttpClient client = HttpClientBuilder.create()
+					.setConnectionReuseStrategy(NoConnectionReuseStrategy.INSTANCE)
+					.build();
+			HttpPost post = new HttpPost(stubUrl);
+			// post.addHeader(arg0, arg1);
+			Map<String, String> headers = getIndexedParameters();
+			for (String name : headers.keySet()) {
+				post.addHeader(name, headers.get(name));
+			}
+			post.setEntity(new StringEntity( mockPreparedStatement.getSQL() ));
+			ResponseHandler<String> handler = new ResponseHandler<String>() {
+				@Override
+				public String handleResponse(final HttpResponse response) throws ClientProtocolException, IOException {
+					int status = response.getStatusLine().getStatusCode();
+					HttpEntity entity = response.getEntity();
+					String body = null;
+					if( entity != null ) {
+						body = EntityUtils.toString(entity);
+					}
+					if( status != 200 ) {
+						throw new RuntimeException("No stub for url: '" + stubUrl + "' body: '" + body + "' found.");
+					}
+					return body;
+				}
+			};
+			return client.execute(post, handler);
+		}
 
-      private static String bodyToString( final Request request ) {
-         if ( request.body() == null ) {
-            return "";
-         }
-         try {
-            final Request copy = request.newBuilder().build();
-            final Buffer buffer = new Buffer();
-            copy.body().writeTo( buffer );
-            return buffer.readUtf8();
-         } catch ( final IOException e ) {
-            return "Failed to extract body from request";
-         }
-      }
+		private Map<String, String> getIndexedParameters() {
+			Set<Map.Entry<ParameterReference, Object>> entries = mockPreparedStatement.getIndexedParameterMap()
+					.entrySet();
 
-      private Map<String, String> getIndexedParameters() {
-         Set<Map.Entry<ParameterReference, Object>> entries = mockPreparedStatement
-               .getIndexedParameterMap().entrySet();
+			if (entries.isEmpty()) {
+				return new HashMap<>();
+			}
+			return entries.stream().collect(Collectors.toMap(entry -> String.valueOf(getIndex(entry.getKey())),
+					entry -> entry.getValue() != null ? entry.getValue().toString() : "null"));
+		}
 
-         if ( entries.isEmpty() ) {
-            return new HashMap<>();
-         }
-         return entries
-               .stream().collect( Collectors.toMap( entry -> String.valueOf( getIndex( entry.getKey() ) ),
-                     entry -> entry.getValue() != null ? entry.getValue().toString() : "null" ) );
-      }
-
-      private static String getIndex( ParameterReference parameterReference ) {
-         String indexValue = parameterReference.toString()
-                                               .replace( "ParameterIndex{index=", "" )
-                                               .replace( "}", "" );
-         return String.valueOf( Integer.parseInt( indexValue ) );
-      }
-   }
+		private static String getIndex(ParameterReference parameterReference) {
+			String indexValue = parameterReference.toString().replace("ParameterIndex{index=", "").replace("}", "");
+			return String.valueOf(Integer.parseInt(indexValue));
+		}
+	}
 
 }
